6e6a35c484e341e113a78d2ce709ef05d6e4b0c0
Ivan Alejandro <ivanalejandro0@gmail.com>
2022-08-20 19:36:27 -0300

    Cleanup styling

--- apps/examples/src/App.tsx
+++ apps/examples/src/App.tsx
@@ -7,20 +7,19 @@ import styles from "./App.module.css";
 
 export default function App() {
   return (
-    <>
-      <div className={styles.container}>
-        <h1>Time control</h1>
-        <p>Usually seen on video and audio players.</p>
-        <TimeIndicator />
-        <h1>Volume control</h1>
-        <Volume />
-        <h1>Image comparison with split</h1>
-        <p>Helpful widget so easily show differences between two pictures.</p>
-        <ImageSideSplit />
-        <h1>Debug widget</h1>
-        <p>Used to show the data we get from the hook.</p>
-        <Debug />
-      </div>
-    </>
+    <div className={styles.container}>
+      <h1>Example widgets created with usePointerDrag</h1>
+      <h2>Time control</h2>
+      <p>Usually seen on video and audio players.</p>
+      <TimeIndicator />
+      <h2>Volume control</h2>
+      <Volume />
+      <h2>Image comparison with split</h2>
+      <p>Helpful widget so easily show differences between two pictures.</p>
+      <ImageSideSplit />
+      <h2>Debug widget</h2>
+      <p>Used to show the data we get from the hook.</p>
+      <Debug />
+    </div>
   );
 }
