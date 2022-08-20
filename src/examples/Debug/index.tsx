1f88db25dc8d65153ec411800d9011b0c232ecf7
Ivan Alejandro <ivanalejandro0@gmail.com>
2022-08-19 23:55:15 -0300

    Keep all data in one state

--- src/examples/Debug/index.tsx
+++ src/examples/Debug/index.tsx
@@ -3,29 +3,34 @@ import { usePointerDrag } from "../../use-pointer-drag";
 
 import styles from "./index.module.css";
 
-type Offset = Parameters<Parameters<typeof usePointerDrag>[0]>[3];
+type Data = {
+  x: number,
+  y: number,
+  top: number,
+  left: number,
+  height: number,
+  width: number,
+  dragging: boolean,
+}
 
 export function Debug() {
-  const [position, setPosition] = useState({x: 0, y: 0});
-  const [dragging, setDragging] = useState<boolean>(false);
-  const [offset, setOffset] = useState<Offset | undefined>();
-
+  const [data, setData] = useState<Data>();
   const ref = usePointerDrag(
-    useCallback((isDragging, x, y, offset) => {
-      setPosition({x, y});
-      setDragging(isDragging);
-      setOffset(offset)
+    useCallback((dragging, x, y, offset) => {
+      setData({x, y, dragging, ...offset})
     }, [])
   );
+
+  const { dragging, x, y, top, left, height, width } = data || {};
 
   return (
     <div>
       <div ref={ref} className={styles.container}>
       </div>
       <div>Dragging: {dragging ? 'true' : 'false'}</div>
-      <div>Pointer position: (x = {position.x}, y = {position.y})</div>
-      <div>top = {offset?.top}, left = {offset?.left}</div>
-      <div>width = {offset?.width}, height = {offset?.height}</div>
+      <div>Pointer position: (x = {x}, y = {y})</div>
+      <div>top = {top}, left = {left}</div>
+      <div>width = {width}, height = {height}</div>
     </div>
   );
 }
