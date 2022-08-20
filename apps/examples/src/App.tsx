a3ea231265aeb31550801cc7cfc91727673124d9
Ivan Alejandro <ivanalejandro0@gmail.com>
2022-08-20 19:23:50 -0300

    Add some text to the example app

--- apps/examples/src/App.tsx
+++ apps/examples/src/App.tsx
@@ -9,9 +9,16 @@ export default function App() {
   return (
     <>
       <div className={styles.container}>
+        <h1>Time control</h1>
+        <p>Usually seen on video and audio players.</p>
         <TimeIndicator />
+        <h1>Volume control</h1>
         <Volume />
+        <h1>Image comparison with split</h1>
+        <p>Helpful widget so easily show differences between two pictures.</p>
         <ImageSideSplit />
+        <h1>Debug widget</h1>
+        <p>Used to show the data we get from the hook.</p>
         <Debug />
       </div>
     </>
